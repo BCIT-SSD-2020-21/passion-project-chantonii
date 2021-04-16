86cc8e1be3e4312c88136d28bb34e9ad79531442
Anthony Chan <achan437@my.bcit.ca>
2021-04-15 20:29:08 -0700

    add nav to go login/registration page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import Gameboard from './Gameboard'
 import './App.css';
+import {BrowserRouter as Router, Route,Switch,Link} from "react-router-dom"
+import Registration from "./Registration"
+import Login from "./Login"
+import Home from './Home'
 
 function App() {
-
+  
   return (
-    <div className="App">
-      <h1>Pacman Clone</h1>
-      <Gameboard></Gameboard>      
+  <Router>
+    <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/Login'>Login</Link></li>
+      </ul>
     </div>
+    <Switch>    
+      <Route exact path="/"> <Home/> </Route>
+      <Route exact path="/register"> <Registration/> </Route>
+      <Route exact path="/login"> <Login/> </Route>
+    </Switch>
+  </Router>
   );
 }
 
---
+++ src/Home/index.js
@@ -0,0 +1,12 @@
+import Gameboard from '../Gameboard'
+import '../App.css';
+
+export default function Home(){
+    return(
+        <div className="App">
+            <h1>Pacman Clone</h1>
+            <Gameboard></Gameboard>  
+        </div>        
+    )
+
+}
