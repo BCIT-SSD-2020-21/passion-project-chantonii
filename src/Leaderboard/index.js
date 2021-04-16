6ab3a15121a384bafc1e5552150cd1dab55c33cd
Anthony Chan <achan437@my.bcit.ca>
2021-04-16 10:02:10 -0700

    username doesn't show entire email

--- src/Leaderboard/index.js
+++ src/Leaderboard/index.js
@@ -5,7 +5,7 @@ export default function Leaderboard(props){
     const points = props.points
 
     return (
-        <div id="gameBoard" style={{width: boardWidth, height: boardHeight, borderWidth: boardWidth/2, background:"black", marginLeft: "auto", marginRight: "auto"}}>
+        <div id="leaderBoard" style={{width: boardWidth, height: boardHeight, borderWidth: boardWidth/2, background:"black", marginLeft: "auto", marginRight: "auto"}}>
             <h2 style={{color:"white"}}>Leaderboard: </h2>
             {points.map((point => {
                 return <p style={{color:"white"}}> {point.name}: {point.score}</p>
