31e00daee9c5e90e005332b7b1ea7a8a7650252a
Anthony Chan <achan437@my.bcit.ca>
2021-04-15 22:25:30 -0700

    add get user if logged in

--- src/App.js
+++ src/App.js
@@ -3,21 +3,38 @@ import {BrowserRouter as Router, Route,Switch,Link} from "react-router-dom"
 import Registration from "./Registration"
 import Login from "./Login"
 import Home from './Home'
+import {useState} from 'react'
+import firebase from './firebase/config.js'
 
 function App() {
+  const [user,setUser] = useState()
+    
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          setUser(user)
+        } else {
+          console.log("no user")
+        }
+      });
   
   return (
   <Router>
     <div>
       <ul>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/Login'>Login</Link></li>
+        {user?
+          <li><Link to='/Login'>Login</Link></li>
+          :
+          <li><Link to="/Logout">Logout</Link></li>
+        }
+        
       </ul>
     </div>
     <Switch>    
       <Route exact path="/"> <Home/> </Route>
       <Route exact path="/register"> <Registration/> </Route>
       <Route exact path="/login"> <Login/> </Route>
+      <Route exact path="/logout"></Route>
     </Switch>
   </Router>
   );
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,13 +1,8 @@
 import Gameboard from '../Gameboard'
 import '../App.css';
-import {useState} from 'react'
 
-export default function Home(props){
-    const [user,setUser] = useState()
-    if(props.user){
-        setUser(props.user)
-        console.log(user)
-    }
+export default function Home(){
+    
     return(
         <div className="App">
             <h1>Pacman Clone</h1>
