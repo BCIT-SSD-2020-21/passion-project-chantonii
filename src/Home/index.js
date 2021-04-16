4baa87ed2ff7c0a240e349f6961a3f178ed8959a
Anthony Chan <achan437@my.bcit.ca>
2021-04-15 22:01:02 -0700

    registration goes to firebase

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,7 +1,13 @@
 import Gameboard from '../Gameboard'
 import '../App.css';
+import {useState} from 'react'
 
-export default function Home(){
+export default function Home(props){
+    const [user,setUser] = useState()
+    if(props.user){
+        setUser(props.user)
+        console.log(user)
+    }
     return(
         <div className="App">
             <h1>Pacman Clone</h1>
